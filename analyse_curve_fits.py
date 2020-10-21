0823d94344c2e976f10d8af160adca03d89b9a44
uditvyas <uditvyas23@gmail.com>
2020-10-21 16:57:16 +0530

    minor tqdm

--- analyse_curve_fits.py
+++ analyse_curve_fits.py
@@ -1,6 +1,6 @@
 import numpy as np
 import paths
-import tqdm.tqdm as tqdm
+from tqdm as tqdm
 from analyse_curve_fit_utils import *
 
 all_2D_params = np.load(params_2D_save_dir+'all_2D_params.npy')
