492722d8e7bf667487fc16fcc38e6a546f761372
uditvyas <uditvyas23@gmail.com>
2020-10-21 17:08:38 +0530

    index minor

--- analyse_curve_fits.py
+++ analyse_curve_fits.py
@@ -16,7 +16,7 @@ for i in tqdm(range(num_images)):
     y_all = []
     mse_all = []
     for j in range(len(profiles)):
-        y, mse = cmp_fit(profiles[i], params[i])
+        y, mse = cmp_fit(profiles[j], params[j])
         y_all.append(y)
         mse_all.append(mse)
     save_cmp(y_all,profiles,mse_all,names[i],cmp_2D_save_dir)
