ed900ce867314e0a473b5b8ffdb743c1baf77cf7
uditvyas <uditvyas23@gmail.com>
2020-10-28 19:00:14 +0530

    minor again

--- compare_profiles.py
+++ compare_profiles.py
@@ -9,7 +9,7 @@ all_profiles = []
 
 for name in names:
     nm = name.split('.')[0]
-    all_profiles.append(np.load(all_profiles_save_dir+'profiles_dem_{}.npy').format(nm))
+    all_profiles.append(np.load(all_profiles_save_dir+'profiles_dem_{}.npy'.format(nm)))
 print(len(all_profiles))
 count = 0
 for i in range(len(names)):
