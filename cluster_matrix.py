4799f249ba86321d2472faee8d58f010a7dfd22e
uditvyas <uditvyas23@gmail.com>
2020-10-29 16:13:32 +0530

    minor again

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -2,7 +2,7 @@ from paths import *
 import numpy as np
 from sklearn.cluster import DBSCAN
 
-distance_matrix = np.save(cluster_results_dir+"distance_matrix.npy")
+distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix)
 
 cluster = DBSCAN(min_samples = 100).fit(distance_matrix)
