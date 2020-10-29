2ec0c0dbe0c25846ad98eefceae0e2cdf9b8232e
uditvyas <uditvyas23@gmail.com>
2020-10-29 16:18:15 +0530

    f is single

--- distribute_images.py
+++ distribute_images.py
@@ -14,7 +14,7 @@ for label in range(len(n_clusters)):
 
 for i in tqdm(range(len(labels))):
     which = labels[i]
-    image = names[i] + '.tiff'
+    image = names[i] + '.tif'
     source = image_dir + image
     destination = cluster_results_dir + 'cluster_{}'.format(which) + image
     copy(source,destination)
