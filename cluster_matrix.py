a4edf643825dffcc00b4a1599d92518682f11995
uditvyas <uditvyas23@gmail.com>
2020-10-29 16:11:57 +0530

    distribute

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -6,4 +6,7 @@ distance_matrix = np.save(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix)
 
 cluster = DBSCAN(min_samples = 100).fit(distance_matrix)
-print(cluster.labels_.shape)
+labels = cluster.labels_
+print(labels.shape)
+
+np.save(cluster_results_dir+"labels",labels)
---
+++ distribute_images.py
@@ -0,0 +1,20 @@
+from paths import *
+import numpy as np
+import os
+from shutil import copy
+from tqdm import tqdm
+
+names = np.load(names_dir+"names.npy")
+labels = np.load(cluster_results_dir+"labels.npy")
+
+n_clusters = np.unique(labels)
+
+for label in range(n_clusters):
+    os.mkdir(cluster_results_dir+"cluster_{}".format(label))
+
+for i in tqdm(range(len(labels))):
+    which = labels[i]
+    image = names[i]
+    source = image_dir + image
+    destination = cluster_results_dir + 'cluster_{}'.format(which) + image
+    copy(source,destination)
