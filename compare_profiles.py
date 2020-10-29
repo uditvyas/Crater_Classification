57e6d689809457a6fb53d2c4199c57c06c5bcd4c
uditvyas <uditvyas23@gmail.com>
2020-10-29 15:13:36 +0530

    similarity matrix attempt 1

--- compare_profiles.py
+++ compare_profiles.py
@@ -2,6 +2,7 @@ from paths import *
 import numpy as np
 import matplotlib.pyplot as plt
 from compare_profiles_utils import *
+from tqdm import tqdm
 
 # Distance Matrix
 Matrix = np.zeros((num_images,num_images))
@@ -9,9 +10,13 @@ Matrix = np.zeros((num_images,num_images))
 # Loading the names and Profiles
 names = np.load(names_dir+'names.npy')
 all_profiles = load_profiles(names,all_profiles_save_dir)
+all_2D_params = np.load(params_2D_save_dir+'all_2D_params.npy')
 
-for i in range(len(names)):
-    first_profiles = all_profiles[i]
+for i in tqdm(range(len(names))):
+    first_profiles = all_2D_params[i]
     for j in range(i+1,len(names)):
-        second_profiles = all_profiles[j]
-        Matrix[i][j] = similarity(first_profiles,second_profiles)
+        second_profiles = all_2D_params[j]
+        Matrix[i][j] = similarity(first_profiles,second_profiles,sigma)
+        Matrix[j][i] = Matrix[i][j]
+
+np.save(cluster_results_dir+"distance_matrix.npy")
--- compare_profiles_utils.py
+++ compare_profiles_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.distance import sqeuclidean, chebyshev, cosine
 
 def load_profiles():
     all_profiles = []
@@ -6,7 +7,22 @@ def load_profiles():
         nm = name.split('.')[0]
         all_profiles.append(np.load(all_profiles_save_dir+'profiles_{}.npy'.format(nm)))
     print("Profiles Loaded")
+    
+def compare(first,second,sigma):
+    cosine_distance = cosine(first,second)
+    sqeuclidean_distance = sqeuclidean(first,second)
+    chebyshev_distance = chebyshev(first,second)
+    which = cosine_distance
+    distance = np.exp(-which/(sigma**2))
+    return distance
 
-
-def similarity(first,second):
-    return 0
+def similarity(first,second,sigma):
+    n = first.shape[0]
+    all_scores = []
+    for i in range(n):
+        local_sum = 0
+        for j in range(n):
+            local_sum += compare(first[j],second[(j+i)%n],sigma)
+        all_scores.append(local_sum)
+    distance = np.min(np.array(all_scores))
+    return distance
