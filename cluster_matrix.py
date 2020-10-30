462f739dd3c43063bcbd3d43770ee2d93c2cfc96
viraj2707 <viraj.shah@iitgn.ac.in>
2020-10-30 22:38:53 +0530

    average

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -6,7 +6,7 @@ distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 
 
-cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='complete').fit(distance_matrix)
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='average').fit(distance_matrix)
 labels = cluster.labels_
 print(np.unique(labels,return_counts=True))
 
