55ef349041423b32230185f0e01312fdc0ecdd05
viraj2707 <viraj.shah@iitgn.ac.in>
2020-10-30 22:28:11 +0530

    eps = 0.2

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -6,7 +6,7 @@ distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 
 
-cluster = DBSCAN(eps=3.0,min_samples = 100).fit(distance_matrix)
+cluster = DBSCAN(eps=0.2,min_samples = 100).fit(distance_matrix)
 labels = cluster.labels_
 print(labels)
 
