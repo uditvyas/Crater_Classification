3d6e4294c8f53a85748fd7683adcad9786f4035d
viraj2707 <viraj.shah@iitgn.ac.in>
2020-10-30 22:59:10 +0530

    all is well

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -10,4 +10,13 @@ cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage=
 labels = cluster.labels_
 print(np.unique(labels,return_counts=True))
 
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='single').fit(distance_matrix)
+labels = cluster.labels_
+print(np.unique(labels,return_counts=True))
+
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='complete').fit(distance_matrix)
+labels = cluster.labels_
+print(np.unique(labels,return_counts=True))
+
+
 np.save(cluster_results_dir+"labels",labels)
