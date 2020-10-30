f2d01be63d81fd4897d380f1024e2363a2333365
viraj2707 <viraj.shah@iitgn.ac.in>
2020-10-30 22:35:48 +0530

    single linkage

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -6,7 +6,7 @@ distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 
 
-cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed').fit(distance_matrix)
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='single').fit(distance_matrix)
 labels = cluster.labels_
 print(labels)
 
