fd6d9e966769f115623f31ade5ff4402537b3d0b
viraj2707 <viraj.shah@iitgn.ac.in>
2020-10-30 22:32:45 +0530

    AgglomerativeClustering

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -1,12 +1,12 @@
 from paths import *
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import AgglomerativeClustering
 
 distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 
 
-cluster = DBSCAN(eps=0.2,min_samples = 100).fit(distance_matrix)
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed').fit(distance_matrix)
 labels = cluster.labels_
 print(labels)
 
