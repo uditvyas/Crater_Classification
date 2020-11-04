4fa469a71cd66a5084b10abe930cadf56ea7f390
uditvyas <uditvyas23@gmail.com>
2020-11-04 12:28:49 +0530

    average

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -7,7 +7,7 @@ distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 all_2D_params = np.load(params_2D_save_dir+'all_2D_params.npy')
 
-cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='complete').fit(distance_matrix)
+cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='average').fit(distance_matrix)
 labels = cluster.labels_
 print(np.unique(labels,return_counts=True))
 np.save(cluster_results_dir+"labels",labels)
