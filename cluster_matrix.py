0f3e9ba286af4147295bd5fdd55c020f20398de4
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-04 14:19:18 +0530

    distance thresh = 0.2

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -6,7 +6,7 @@ distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
 
 
-cluster = AgglomerativeClustering(distance_threshold=0.25, affinity='precomputed', linkage='complete').fit(distance_matrix)
+cluster = AgglomerativeClustering(distance_threshold=0.20, affinity='precomputed', linkage='complete').fit(distance_matrix)
 labels = cluster.labels_
 print(np.unique(labels,return_counts=True))
 
