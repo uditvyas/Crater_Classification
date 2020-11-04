cffac1f4d073aa02d79589d11cf37c41c63bb034
uditvyas <uditvyas23@gmail.com>
2020-11-04 12:19:59 +0530

    Attemp to score clusters

--- cluster_matrix.py
+++ cluster_matrix.py
@@ -1,13 +1,14 @@
 from paths import *
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score as sh_score
 
 distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
 print("Distance Matrix Loaded: ",distance_matrix.shape)
-
+all_2D_params = np.load(params_2D_save_dir+'all_2D_params.npy')
 
 cluster = AgglomerativeClustering(n_clusters=8, affinity='precomputed', linkage='complete').fit(distance_matrix)
 labels = cluster.labels_
 print(np.unique(labels,return_counts=True))
-
+print("Silhouette Score: ".format(sh_score(all_2D_params,labels)))
 np.save(cluster_results_dir+"labels",labels)
