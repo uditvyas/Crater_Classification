4d0214e21f79da4eb4a97fa8430ca54067210505
uditvyas <uditvyas23@gmail.com>
2020-11-05 19:18:54 +0530

    resampled profiles

--- paths.py
+++ paths.py
@@ -1,6 +1,7 @@
 image_dir = '/media/HDD2/btech2/Crater/dataset_v1/dems/'
 ortho_dir = '/media/HDD2/btech2/Crater/dataset_v1/orthos/'
 all_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/all/'
+resize_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/resize/'
 mean_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/means/'
 figs_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/figs/'
 params_3D_save_dir = '/media/HDD2/btech2/Crater/Outputs/params3d/'
@@ -10,3 +11,4 @@ cluster_results_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/cluster/'
 names_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/'
 num_images = 2076
 sigma = 100
+resample_dimension = 75
---
+++ resample_profiles.py
@@ -0,0 +1,22 @@
+from paths import *
+import numpy as np
+import matplotlib.pyplot as plt
+from compare_profiles_utils import *
+from tqdm import tqdm
+from scipy import signal
+
+names = np.load(names_dir+'names.npy')
+all_profiles = load_profiles(names,all_profiles_save_dir)
+
+resampled_profiles = []
+
+for profiles in all_profiles:
+    r_profiles = []
+    for profile in profiles:
+        profile = signal.resample(profile, resample_dimension)
+        r_profiles.append(profile)
+    r_profiles = np.array(r_profiles)
+    resampled_profiles.append(r_profiles)
+
+resampled_profiles = np.array(resampled_profiles)
+np.save(resize_profiles_save_dir + "resampled_profiles", resampled_profiles)
