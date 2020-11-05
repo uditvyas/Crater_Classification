7be2aef744541069ef3bd19b4435d05221d9cc7b
uditvyas <uditvyas23@gmail.com>
2020-11-05 19:51:36 +0530

    resampled_distances

--- compare_profiles.py
+++ compare_profiles.py
@@ -11,11 +11,13 @@ Matrix = np.zeros((num_images,num_images))
 names = np.load(names_dir+'names.npy')
 all_profiles = load_profiles(names,all_profiles_save_dir)
 all_2D_params = np.load(params_2D_save_dir+'all_2D_params.npy')
+resampled_profiles = np.load(resize_profiles_save_dir+'resampled_profiles.npy')
+print("Resampled Profiles Loaded: ",resampled_profiles.shape)
 
 for i in tqdm(range(len(names))):
-    first_profiles = all_2D_params[i]
+    first_profiles = resampled_profiles[i]
     for j in range(i+1,len(names)):
-        second_profiles = all_2D_params[j]
+        second_profiles = resampled_profiles[j]
         Matrix[i][j] = similarity(first_profiles,second_profiles,sigma)
         Matrix[j][i] = Matrix[i][j]
 np.save(cluster_results_dir+"distance_matrix.npy", Matrix)
