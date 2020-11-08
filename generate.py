1ac3664b8c09f90281d44b677261e49a7572dda4
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-08 20:08:42 +0530

    minor, role models in changing

--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 from generate_utils import *
 from paths import *
 
-profiles, names = generate_save_profiles(image_dir, all_profiles_save_dir, num_images)
+profiles, names = generate_save_profiles(image_dir, all_profiles_save_dir, num_images, num_profiles)
 np.save(names_dir+"names",np.array(names))
