663d1084bd17c62ead553fb0cb7f4e7ff6f6f5f3
uditvyas <uditvyas23@gmail.com>
2020-11-11 13:17:18 +0530

    checking rim width

--- feature_vector/extract_simple.py
+++ feature_vector/extract_simple.py
@@ -11,8 +11,8 @@ for i in tqdm(range(len(names))):
 
     depth = get_depth(I_dem)
     rim_height = get_rim_height(I_dem, depth)
-    print(depth, rim_height, rim_height/depth)
     rim_width = get_rim_width(I_dem)
+    print(depth, rim_height, rim_width)
     floor_diameter = get_floor_diameter(I_dem)
     interior_volume = get_interior_volume(I_dem)
     if(i==10):
