5f5bc1c478dcbbe5d614f87b6fd76afe23f88968
uditvyas <uditvyas23@gmail.com>
2020-11-11 13:40:07 +0530

    checking negatives

--- feature_vector/extract_simple.py
+++ feature_vector/extract_simple.py
@@ -12,8 +12,9 @@ for i in tqdm(range(len(names))):
     depth = get_depth(I_dem)
     rim_height = get_rim_height(I_dem, depth)
     rim_width = get_rim_width(I_dem)
-    print(depth, rim_height, rim_width)
+    if rim_height<0:
+        print(depth, rim_height, rim_width)
+        rim_height = 0
     floor_diameter = get_floor_diameter(I_dem)
     interior_volume = get_interior_volume(I_dem)
-    if(i==10):
-        break
+    
