628cd549a85b7b91a17f8fcf2711d7374fcddeb0
uditvyas <uditvyas23@gmail.com>
2020-11-16 23:07:00 +0530

    visual 5 clusters

---
+++ feature_vector/cluster.py
@@ -0,0 +1,31 @@
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from paths import *
+
+all_features = np.load(features_save_dir+'all_features.npy')
+
+params = all_features
+km = KMeans(n_clusters=5)
+km_result = km.fit(params)
+labels = km.labels_
+
+tsne = TSNE(n_components=2, verbose=1)
+tsne_results = tsne.fit_transform(params)
+data = pd.DataFrame()
+data['x'] = tsne_results[:,0]
+data['y'] = tsne_results[:,1]
+data['labels'] = labels
+plt.figure(figsize=(15,15))
+sns.scatterplot(x='x', y='y', 
+                palette=sns.color_palette("hls",8),
+                data = data,
+                hue = 'labels',
+                legend = 'full',
+                alpha = 0.3)
+plt.savefig(cluster_results_dir + "visual_cluster_5.jpg".format(j))
+plt.close()
