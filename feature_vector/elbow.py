f9d480b251cea95edaf4859051147761ae773288
uditvyas <uditvyas23@gmail.com>
2020-11-16 23:00:05 +0530

    elbow on normalised feature vector

---
+++ feature_vector/elbow.py
@@ -0,0 +1,28 @@
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+from paths import *
+
+all_features = np.load(features_save_dir+'all_2D_params.npy')
+names = np.load(params_2D_save_dir+'names.npy')
+
+x = []
+y = []
+params = all_features
+
+for i in tqdm(range(2,10)):
+    
+    # initialise and fit the Kmeams model
+    kmeans = KMeans(n_clusters= i, random_state=0)
+    result = kmeans.fit(params)
+    print(result.labels_.shape)
+    # append the number of clusters to x data list
+    x.append(i)
+    # append average within cluster sum of squares to y data set
+    awcss = kmeans.inertia_/params.shape[0]
+    y.append(awcss)
+    print("Clusters = {}\tScore = {}".format(i,awcss))
+plt.plot(x,y,'bo-')
+plt.savefig(cluster_results_dir+"elbow.jpg")
+plt.close()
