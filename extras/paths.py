6eac86690e42b0d7650f56b96d13b6ea2f69e691
uditvyas <uditvyas23@gmail.com>
2020-11-24 17:39:52 +0530

    done!

--- extras/paths.py
+++ extras/paths.py
@@ -2,3 +2,4 @@ names_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/'
 params_2D_save_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 cluster_results_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 all_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/all/'
+num_images = 2076
