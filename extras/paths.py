23d9916931e288faaebe17ab78d9dd8e07a1ea67
uditvyas <uditvyas23@gmail.com>
2020-11-24 17:49:58 +0530

    sigma added

--- extras/paths.py
+++ extras/paths.py
@@ -3,3 +3,4 @@ params_2D_save_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 cluster_results_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 all_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/all/'
 num_images = 2076
+sigma = 5
