a34f75e036625e617e9f60a644450457b4fb0e8c
uditvyas <uditvyas23@gmail.com>
2020-11-24 17:45:46 +0530

    cluster for params

---
+++ extras/cluster_matrix.py
@@ -0,0 +1,18 @@
+from paths import *
+import numpy as np
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import silhouette_score as sh_score
+
+distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
+
+print("Distance Matrix Loaded: ",distance_matrix.shape)
+
+cluster = AgglomerativeClustering(n_clusters = None, distance_threshold=0.75, affinity='precomputed', linkage='complete').fit(distance_matrix)
+print("Maximum of Distance Matrix is {}".format(np.max(distance_matrix)))
+print("Minimum of Distance Matrix is {}".format(np.min(distance_matrix)))
+print("Average of Distance Matrix is {}".format(np.mean(distance_matrix)))
+print("Median of Distance Matrix is {}".format(np.median(distance_matrix)))
+labels = cluster.labels_
+print(np.unique(labels,return_counts=True))
+np.save(cluster_results_dir+"labels",labels)
+print("Silhouette Score: {}".format(sh_score(distance_matrix, labels, metric = "precomputed")))
