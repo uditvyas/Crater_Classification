54a951571c019261a8984d5411563e78eae2041c
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-25 15:07:52 +0530

    first time from server

--- profile_analysis/paths.py
+++ profile_analysis/paths.py
@@ -11,5 +11,5 @@ cluster_results_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/cluster/'
 names_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/'
 num_images = 2076
 sigma = 250
-resample_dimension = 75
+resample_dimension = 200
 num_profiles = 15
