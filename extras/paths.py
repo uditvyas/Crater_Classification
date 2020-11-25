6ee2707969a469fd9477c22cc52633a900cd3cc6
uditvyas <uditvyas23@gmail.com>
2020-11-25 15:36:56 +0530

    ortho dir added in extras

--- extras/paths.py
+++ extras/paths.py
@@ -1,5 +1,6 @@
 names_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 params_2D_save_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
+ortho_dir = '/media/HDD2/btech2/Crater/dataset_v1/orthos/'
 cluster_results_dir = '/media/HDD2/btech2/Crater/Outputs/params_2D/'
 all_profiles_save_dir = '/media/HDD2/btech2/Crater/Outputs/profiles/all/'
 num_images = 2076
