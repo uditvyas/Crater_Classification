8829690cd8f838c1b38e69776b6b20c92223c0de
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-25 16:09:07 +0530

    faster compare?

--- profile_analysis/compare_profiles_utils.py
+++ profile_analysis/compare_profiles_utils.py
@@ -9,7 +9,7 @@ def load_profiles(names,directory):
         all_profiles.append(np.load(directory+'profiles_{}.npy'.format(nm)))
     print("Profiles Loaded")
     return all_profiles
-    
+'''
 def compare(first,second,sigma):
     # cosine_distance = cosine(first,second)
     euclidean_distance = euclidean(first,second)
@@ -17,7 +17,7 @@ def compare(first,second,sigma):
     which = euclidean_distance
     distance = 1 - np.exp(-which/(sigma**2))
     return distance
-    
+  
 def similarity(first,second,sigma):
     n = first.shape[0]
     all_scores = []
@@ -26,5 +26,25 @@ def similarity(first,second,sigma):
         for j in range(n):
             local_sum += compare(first[j],second[(j+i)%n],sigma)
         all_scores.append(local_sum/num_profiles)
+    distance = np.min(np.array(all_scores))
+    return distance
+'''
+def compare(first,second,sigma):
+    # cosine_distance = cosine(first,second)
+    euclidean_distance = (first-second)**2
+    euclidean_distance = np.sum(euclidean_distance)
+    # chebyshev_distance = chebyshev(first,second)
+    which = euclidean_distance
+    distance = 1 - np.exp(-which/(sigma**2))
+    return distance
+
+def similarity(first,second,sigma):
+    n = first.shape[0]
+    all_scores = []
+    print(first.shape,second.shape)
+    for i in range(n):
+        second_c = np.vstack(second[i:n,:],second[0:i,:])
+        local_sum = compare(first,second_c,sigma)
+        all_scores.append(local_sum)
     distance = np.min(np.array(all_scores))
     return distance
