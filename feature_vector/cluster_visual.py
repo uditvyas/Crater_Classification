996b3152a1271ae905534a11c2ab720980020e77
viraj2707 <viraj.shah@iitgn.ac.in>
2020-11-27 22:41:03 +0530

    visual f_V

---
+++ feature_vector/cluster_visual.py
@@ -0,0 +1,33 @@
+from tqdm import tqdm
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from paths import *
+
+all_features = np.load(features_save_dir+'all_2D_params.npy')
+
+labels = np.load(params_2D_save_dir+'labels.npy')
+
+params = all_features
+
+tsne = TSNE(n_components=2, verbose=1)
+tsne_results = tsne.fit_transform(params)
+
+data = pd.DataFrame()
+data['x'] = tsne_results[:,0]
+data['y'] = tsne_results[:,1]
+data['labels'] = labels
+
+plt.figure(figsize=(15,15))
+sns.scatterplot(x='x', y='y', 
+                palette=sns.color_palette("hls",len(np.unique(labels))),
+                data = data,
+                hue = 'labels',
+                legend = 'full',
+                alpha = 0.3)
+plt.savefig(cluster_results_dir + "visual.jpg")
+plt.close()
+
