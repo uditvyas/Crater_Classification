436ff4f0f0d78afa5f101470b4a94ec623b6f1e2
uditvyas <uditvyas23@gmail.com>
2020-11-29 00:32:55 +0530

    dendogram minor

--- feature_vector/dendogram.py
+++ feature_vector/dendogram.py
@@ -2,13 +2,14 @@ import numpy as np
 from paths import *
 from scipy.cluster.hierarchy import dendrogram, linkage
 from scipy.spatial.distance import squareform
+from sklearn.metrics import pairwise_distances
 
 import matplotlib.pyplot as plt
 
 all_features = np.load(features_save_dir+'all_features.npy')
 labels = np.load(features_save_dir+'labels.npy')
 
-distance_matrix = (all_features, metric = 'sqeuclidean')
+distance_matrix = pairwise_distances(all_features, metric = 'sqeuclidean')
 distance_matrix = 1 - np.exp(distance_matrix)
 
 dists = squareform(all_features)
