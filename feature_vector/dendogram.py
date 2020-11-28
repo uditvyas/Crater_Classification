f504bb628d39e0dd122d25218a03842e618bb3e7
uditvyas <uditvyas23@gmail.com>
2020-11-29 00:27:39 +0530

    dendogram

---
+++ feature_vector/dendogram.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.spatial.distance import squareform
+
+import matplotlib.pyplot as plt
+
+all_features = np.load(features_save_dir+'all_features.npy')
+labels = np.load(features_save_dir+'labels.npy')
+
+distance_matrix = (all_features, metric = 'sqeuclidean')
+distance_matrix = 1 - np.exp(distance_matrix)
+
+dists = squareform(all_features)
+linkage_matrix = linkage(dists, "single")
+dendrogram(linkage_matrix, labels=labels)
+plt.savefig(cluster_results_dir + "dendogram.jpg")
+plt.show()
---
+++ profile_analysis/dendogram.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.spatial.distance import squareform
+
+import matplotlib.pyplot as plt
+
+distance_matrix = np.load(cluster_results_dir+"distance_matrix.npy")
+labels = np.load(cluster_results_dir+"labels.npy")
+
+dists = squareform(distance_matrix)
+linkage_matrix = linkage(dists, "complete")
+dendrogram(linkage_matrix, labels=labels)
+plt.savefig(cluster_results_dir + "dendogram.jpg")
+plt.show()
